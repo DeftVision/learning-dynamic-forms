67a6ad0f74f08d44302842edc73112e4a8397401
Gabriel <gjbarret@asu.edu>
2024-08-20 12:26:04 -0600

    hello world application
    exercise for day 1-2

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        Hello World
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+           ERROR 404: NOT FOUND
+        </div>
+    );
+};
+
+export default Error;
